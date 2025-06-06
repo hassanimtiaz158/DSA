72d1047df1a414fd7304c3649bf77ce584184590
Malik <hassanimtiaz158@gmail.com>
2025-06-06 19:56:20 +0500

    Bubble Sort
    Sort array using bubble sort algorithm with optimized approach.

---
+++ bubblesort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void bubblesort(int arr[],int size){
+	int swaps=0;
+	for(int i=0;i<size;i++){
+		bool swapped=false;
+		for(int j=0;j<size-1;j++){
+			if(arr[j]>arr[j+1]){
+				int temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+				swaps++;
+				swapped=true;
+			}
+		}
+		if(!swapped){
+			break;
+		}
+	}
+	cout<<"Sorted array: ";
+	for(int i=0;i<size;i++){
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+	cout<<"Swaps: "<<swaps<<endl;
+}
+
+int main(){
+	int size;
+	cout<<"Size: ";
+	cin>>size;
+	int arr[size];
+	for(int i=0;i<size;i++){
+		cout<<"Enter element "<<i<<": ";
+		cin>>arr[i];
+	}
+	bubblesort(arr,size);
+	
+	return 0;
+}
