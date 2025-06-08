3bbdff5b3655c4f2e596e6c638400151fd005e88
Malik <hassanimtiaz158@gmail.com>
2025-06-08 23:39:18 +0500

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 void bubblesort(int arr[],int size){
 	int swaps=0;
-	for(int i=0;i<size;i++){
+	for(int i=0;i<size-1;i++){
 		bool swapped=false;
-		for(int j=0;j<size-1;j++){
+		for(int j=0;j<size-i-1;j++){
 			if(arr[j]>arr[j+1]){
 				int temp=arr[j];
 				arr[j]=arr[j+1];
