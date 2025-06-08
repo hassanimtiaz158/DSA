530ba3f60700dab969fda311868b7a8d0f88d933
Malik <hassanimtiaz158@gmail.com>
2025-06-08 23:39:54 +0500

    Update stringbubblesort.cpp

--- stringbubblesort.cpp
+++ stringbubblesort.cpp
@@ -10,7 +10,7 @@ bool caseinsensitivitycomparator(char a,char b){
 
 void bubblesort(string&str){
 	int n=str.length();
-	for(int i=0;i<n;i++){
+	for(int i=0;i<n-1;i++){
 		for(int j=0;j<n-i-1;j++){
 			if(caseinsensitivitycomparator(str[j],str[j+1])){
 				std::swap(str[j],str[j+1]);
